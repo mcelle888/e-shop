33df453833f07d355bdd00358b5264c7624c1ce0
Michelle Liang <smlxm08@gmail.com>
2024-04-16 23:13:35 +1000

    added some  styling for landing page

--- src/components/Pages/LandingPage/LandingPage.jsx
+++ src/components/Pages/LandingPage/LandingPage.jsx
@@ -4,11 +4,22 @@ import styles from './LandingPage.module.scss'
 const LandingPage = () => {
   return (
     <main>
-        <div>
+        <div className={styles.imgBox}>
             <img className={styles.banner} src="src\assets\flowerfield.jpg" alt="field" />
         </div>
-        <h1>Flower Haven</h1>
-        <p>Handcrafted bouquets and arrangements to brighten every occassion!</p>
+        <div className={styles.header}>
+            <h1>Flower Haven</h1>
+            <p>Handcrafted bouquets and arrangements for every occassion</p>
+        </div>
+        <article>
+            <h2>Featured Products</h2>
+            <div>
+                IMAGE BOX
+            </div>
+            {/* todo: make this a link rather than a button */}
+            <button>View All Products</button>
+        </article>
+       
     </main>
   )
 }
